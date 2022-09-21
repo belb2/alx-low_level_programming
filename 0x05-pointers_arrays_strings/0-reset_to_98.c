2a95937bf42809e48e86b67cc91b42a3f0d47fed
belab2 <belacomp@gmail.com>
2022-09-21 22:18:39 +0300

    betty error corrected

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 /**
- *reset_to_98 -reset the variable to 98
+ *reset_to_98 - reset the variable to 98
  *@n:input pointer variable
  *Return: nothing
  */
 void reset_to_98(int *n)
 {
-  *n = 98;
+	*n = 98;
 }
